fc8a402a87c2d756ff82a9149fc36f8ba546f857
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-09-10 16:36:32 -0500

    completed exercise

---
+++ src/Pokecard.js
@@ -0,0 +1,17 @@
+import './Pokecard.css'
+
+const Pokecard = ({ id, name, type, exp }) => {
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+  const altText = `image of ${name}`
+  
+  return (
+    <div className="Pokecard">
+      <p>{name}</p>
+      <img src={imgUrl} alt={altText}></img>
+      <sub className="Pokecard-sub">Type: {type}</sub>
+      <sub className="Pokecard-sub">EXP: {exp}</sub>
+    </div>
+  )
+}
+
+export default Pokecard
